a097f0af4b728dc8960f716be6740ac181d20aa6
vuonga1103 <vuonga1103@yahoo.com>
2020-11-03 15:21:04 -0500

    changing get route to return all posts

--- routes/posts.js
+++ routes/posts.js
@@ -6,8 +6,13 @@ const router = express.Router();
 const Post = require("../models/Post");
 
 // we don't need to specify /posts, because everytime we go to /posts this route will run
-router.get("/", (req, res) => {
-  res.send("We are on posts");
+router.get("/", async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.json(posts);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 router.post("/", async (req, res) => {
